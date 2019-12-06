1bfffa27706f8d5839872175635b56a85c5e9404
Reiko Fukuda <strawberryred123@gmail.com>
2019-12-06 16:36:06 -0600

    working

--- script.js
+++ script.js
@@ -20,15 +20,77 @@ window.addEventListener("load", function() {
    //    console.log(input.value);
    // })
 
-   let launchForm = document.getElementById("launchForm");
-   launchForm.addEventListener("submit", function(event) {      
-      let pilotNameInput = document.getElementById("pilotName");
-      let copilotNameInput = document.getElementById("copilotName");
-      let fuelLevelInput = document.getElementById("fuelLevel");
-      let cargoMassInput = document.getElementById("cargoMass");
+   
+   let form = document.getElementById("launchForm");
+
+   let pilotNameInput = document.getElementById(id="pilotName");
+   let copilotNameInput = document.getElementById(id="copilotName");
+   let fuelLevelInput = document.getElementById(id="fuelLevel");
+   let cargoMassInput = document.getElementById(id="cargoMass");
+   
+   form.addEventListener("submit", function(event) {      
+
+      //newFunction(pilotNameInput, copilotNameInput, fuelLevelInput, cargoMassInput);
+
       if (pilotNameInput === "" || copilotNameInput === "" || fuelLevelInput === "" || cargoMassInput === "") 
          alert("All fields are required");
-      event.preventDefault();
+      
+      if (pilotNameInput != String() || copilotNameInput != String())
+         alert("Input shoud be a string.");
+
+      if (fuelLevelInput != Number() || cargoMassInput != Number())
+         alert("Input shoud be a number.")
+
    })
-   alert();
+
+   // let statusCheck = document.getElementById("faultyItems");
+   // statusCheck.addEventListener("load", function(event) {
+      
+   // })
+
+   // let pilotCheck = document.getElementById("pilotStatus");
+   // pilotCheck.addEventListener("", function(event) {
+
+   // })
+
+   // let copilotCheck = document.getElementById("copolotStatus");
+   // copilotCheck.addEventListener("", function(event) {
+
+   // })
+ 
+   let launchStatus = document.getElementById("launchStatus");
+
+   let fuelCheck = document.getElementById("fuelLevel");
+   
+   let fuelStatus = document.getElementsById("fuelStatus");
+
+   let cargoCheck = document.getElementById("cargoMass");
+
+   let cargoStatus = document.getElementById("cargoStatus");
+
+
+   if (fuelLevelInput.value < 10000) {
+      document.getElementById("fuelStatus").style.visibility = "visible";      
+
+      fuelCheck.addEventListener("click", function changeText(event) {
+         
+         fuelStatus.textContent = "There is not enough fuel for the journey.";                
+      })
+   }   
+   
+   if (cargoMassInput.value > 10000) {
+      document.getElementById("fuelStatus").style.visibility = "visible";
+
+      cargoCheck.addEventListener("click", function changeText(event) {         
+         cargoStatus.textContent = "There is too much mass for the shuttle to take off.";
+      })
+
+   }      
+
+   if (fuelLevelInput.value < 10000 && cargoMassInput.value > 10000) {      
+      launchStatus.textContent = "Shuttle not ready for launch";
+      launchStatus.style.color = 'red';
+   }
+
+   event.preventDefault();
 })
