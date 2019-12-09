0dd3056d72223c00118a6baff1687cf4a6d8adcf
Reiko Fukuda <strawberryred123@gmail.com>
2019-12-08 23:25:37 -0600

    working

--- script.js
+++ script.js
@@ -25,94 +25,97 @@ window.addEventListener("load", function () {
 
       //newFunction(pilotNameInput, copilotNameInput, fuelLevelInput, cargoMassInput);
 
-      if (pilotNameInput === "" || copilotNameInput === "" || fuelLevelInput === "" || cargoMassInput === "") 
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") 
          alert("All fields are required");
       
-      if (pilotNameInput != String() || copilotNameInput != String())
-      //if (isNaN(pilotNameInput || copilotNameInput)===false)
-         alert("Input shoud be a string.");
+      if (!= isNaN(pilotNameInput.value) || != isNaN(copilotNameInput.value))
+         alert("String shoud be used for name.");
+     
+      if (isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value))
+         alert("Fuel / Mass shoud be a number.");
 
-      if (fuelLevelInput != Number() || cargoMassInput != Number())
-      //if (isNaN(fuelLevelInput || cargoMassInput)===true)
-         alert("Input shoud be a number.");         
-   })
+   console.log(pilotNameInput.value);
+   console.log(typeof pilotNameInput.value);
+   
+
    event.preventDefault();
+   })
 })
 
-   // let statusCheck = document.getElementById("faultyItems");
-   // statusCheck.addEventListener("load", function(event) {      
+//    // let statusCheck = document.getElementById("faultyItems");
+//    // statusCheck.addEventListener("load", function(event) {      
 
-let pilotCheck = document.getElementById("pilotStatus");
-pilotCheck.addEventListener("load", function(event) {
-   pilotStatus.innerHTML = `Pilot ${pilotNameInput }Ready`
-   console.log(pilotStatus.innerHTML);
-})
+// let pilotCheck = document.getElementById("pilotStatus");
+// pilotCheck.addEventListener("load", function(event) {
+//    pilotStatus.innerHTML = `Pilot ${pilotNameInput }Ready`
+//    console.log(pilotStatus.innerHTML);
+// })
 
-let copilotCheck = document.getElementById("copilotStatus");
-copilotCheck.addEventListener("load", function(event) {
-   copilotStatus.innerHTML = `Copilot ${copilotNameInput }Ready`
-})
+// let copilotCheck = document.getElementById("copilotStatus");
+// copilotCheck.addEventListener("load", function(event) {
+//    copilotStatus.innerHTML = `Copilot ${copilotNameInput }Ready`
+// })
 
-   //let launchStatusCheck = document.getElementById("launchStatus");
+//    //let launchStatusCheck = document.getElementById("launchStatus");
 
-let fuelCheck = document.getElementById("fuelLevel");
-let fuelStatus = document.getElementById("fuelStatus");
+// let fuelCheck = document.getElementById("fuelLevel");
+// let fuelStatus = document.getElementById("fuelStatus");
 
-if (fuelLevelInput.value < 10000) {             
-   fuelCheck.addEventListener("submit", function changeText(event) {         
-   fuelStatus.textContent = "There is not enough fuel for the journey.";
-   document.getElementById("fuelStatus").style.visibility = "visible";            
-   })
-}
+// if (fuelLevelInput.value < 10000) {             
+//    fuelCheck.addEventListener("submit", function changeText(event) {         
+//    fuelStatus.textContent = "There is not enough fuel for the journey.";
+//    document.getElementById("fuelStatus").style.visibility = "visible";            
+//    })
+// }
 
-let cargoCheck = document.getElementById("cargoMass");
-let cargoStatus = document.getElementById("cargoStatus");   
+// let cargoCheck = document.getElementById("cargoMass");
+// let cargoStatus = document.getElementById("cargoStatus");   
 
-if (cargoMassInput.value > 10000) {
-   cargoCheck.addEventListener("submit", function changeText(event) {         
-   cargoStatus.textContent = "There is too much mass for the shuttle to take off.";         
-   document.getElementById("cargoStatus").style.visibility = "visible";
-   })
-}      
+// if (cargoMassInput.value > 10000) {
+//    cargoCheck.addEventListener("submit", function changeText(event) {         
+//    cargoStatus.textContent = "There is too much mass for the shuttle to take off.";         
+//    document.getElementById("cargoStatus").style.visibility = "visible";
+//    })
+// }      
 
-if (fuelLevelInput.value < 10000 || cargoMassInput.value > 10000) {   
-   launchStatus.addEventListener("submit", function changeText(event) {
-      launchStatus.textContent = "Shuttle not ready for launch";
-      launchStatus.style.color = 'red';
-   })
-}
+// if (fuelLevelInput.value < 10000 || cargoMassInput.value > 10000) {   
+//    launchStatus.addEventListener("submit", function changeText(event) {
+//       launchStatus.textContent = "Shuttle not ready for launch";
+//       launchStatus.style.color = 'red';
+//    })
+// }
 
-if (fuelLevelInput.value > 10000 || cargoMassInput.value < 10000) {   
-   launchStatus.addEventListener("submit", function changeText(event) {
-      launchStatus.textContent = "Shuttle is ready for launch";
-      launchStatus.style.color = 'green';
-   })
-}
+// if (fuelLevelInput.value > 10000 || cargoMassInput.value < 10000) {   
+//    launchStatus.addEventListener("submit", function changeText(event) {
+//       launchStatus.textContent = "Shuttle is ready for launch";
+//       launchStatus.style.color = 'green';
+//    })
+// }
 
 
-window.addEventListener("load", function(event) {
-   let json = [];
-   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-      response.json().then(function(json) {
-         let missionDestination = getElementById("missionTarget");
-         missionTarget.addEventListener("click", function() {
-            for (let i=0; i<missionTarget.length; i++) {
-               missionTarget.innerHTML = `         
+// window.addEventListener("load", function(event) {
+//    let json = [];
+//    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+//       response.json().then(function(json) {
+//          let missionDestination = getElementById("missionTarget");
+//          missionTarget.addEventListener("click", function() {
+//             for (let i=0; i<missionTarget.length; i++) {
+//                missionTarget.innerHTML = `         
                
-               <h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${json.name[i]}</li>
-                  <li>Diameter: ${json.diameter[i]}</li>
-                  <li>Star: ${json.star[i]}</li>
-                  <li>Distance from Earth: ${json.distance[i]}</li>
-                  <li>Number of Moons: ${json.moons[i]}</li>
-               </ol>
-               <img src="${jsom.image[i]}">   
-               `;
-            }
-         })
-      })
-   })
-console.log(json[1]); 
-})
+//                <h2>Mission Destination</h2>
+//                <ol>
+//                   <li>Name: ${json.name[i]}</li>
+//                   <li>Diameter: ${json.diameter[i]}</li>
+//                   <li>Star: ${json.star[i]}</li>
+//                   <li>Distance from Earth: ${json.distance[i]}</li>
+//                   <li>Number of Moons: ${json.moons[i]}</li>
+//                </ol>
+//                <img src="${jsom.image[i]}">   
+//                `;
+//             }
+//          })
+//       })
+//    })
+// console.log(json[1]); 
+// })
     
